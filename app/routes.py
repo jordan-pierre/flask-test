9843f52d2872590861729e38c0fca9ababf1e20e
Jordan <jordan.j.pierre@gmail.com>
2020-11-14 14:03:55 -0500

    Added a template

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Jordan'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Test Site</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
